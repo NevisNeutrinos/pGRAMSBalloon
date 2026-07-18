10b2303fd97ea0bd305c4651c0cdc4833b2248fd
Yinrui-Liu <liuyinrui16@mails.ucas.ac.cn>
2026-07-17 21:08:17 -0400

    [WIP] adding full event data (using pGramsFC 2.0.0 as base, GUI buttons to be added)

--- include/CommunicationCodes.hh
+++ include/CommunicationCodes.hh
@@ -92,6 +92,7 @@ enum class CommunicationCodes : uint16_t {
   TPCMonitor_Query_Event_Data = construct_code(0x8, COM_SUBSYSTEM_TPCMonitor_MSK),
   TPCMonitor_Start_Continuous_LBW = construct_code(0x9, COM_SUBSYSTEM_TPCMonitor_MSK),
   TPCMonitor_Stop_Continuous_LBW = construct_code(0xA, COM_SUBSYSTEM_TPCMonitor_MSK),
+  TPCMonitor_Send_Full_Event_Data = construct_code(0xB, COM_SUBSYSTEM_TPCMonitor_MSK),
 
   // TOF
   TOF_Start_DAQ = construct_code(0x0, COM_SUBSYSTEM_TOF_MSK),
